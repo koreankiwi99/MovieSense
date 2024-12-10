46370d5b427e0bf2a3ccaaf4fa1ff738c2e33de7
koreankiwi99 <salgu@snu.ac.kr>
2024-12-10 11:59:00 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 from unicodedata import normalize
 import regex as re
-from typing import List
+from typing import List, Dict
 
 def clean(text: str) -> str:
     """
@@ -15,3 +15,38 @@ def filter_texts(texts: List[str]) -> List[str]:
     Filters texts containing only Unicode letters.
     """
     return [t for t in map(clean, texts) if not re.search(r'[^\p{L}]', t)]
+
+def show_wordcloud(male_bag_of_words : Dict[str, int],
+                   female_bag_of_words : Dict[str, int],
+                   title : str,
+                   max_words : int = 100):
+  wordcloud_female = WordCloud(
+    width=400, 
+    height=400, 
+    background_color='white',
+    colormap = 'Reds',
+    ).generate_from_frequencies(female_bag_of_words)
+  
+  wordcloud_male = WordCloud(
+    width=400, 
+    height=400, 
+    background_color='white',
+    colormap = 'Blues',
+    ).generate_from_frequencies(male_bag_of_words)
+
+  # Display the word clouds side by side
+  fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+  fig.suptitle(title, fontsize=16)
+
+  # Female verbs word cloud
+  axes[0].imshow(wordcloud_female, interpolation='bilinear')
+  axes[0].set_title("Female Characters")
+  axes[0].axis('off')
+
+  # Male verbs word cloud
+  axes[1].imshow(wordcloud_male, interpolation='bilinear')
+  axes[1].set_title("Male Characters")
+  axes[1].axis('off')
+
+  plt.tight_layout()
+  plt.show()
